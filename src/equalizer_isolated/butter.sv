a565fcad3dc272642f9942a60dadafeb5b941a49
Tasmin Khan <tasminkhanelec@gmail.com>
2025-09-25 04:26:34 +0600

    Write registers memory mapping has been done, no read register has been implemented

--- src/equalizer_isolated/butter.sv
+++ src/equalizer_isolated/butter.sv
@@ -1,8 +1,14 @@
 module butter #(parameter N=2, parameter width = 16)(
 input logic clk,
 input logic rst,
-input logic [width-1:0] xin,
-output logic [width-1:0] yout
+output logic [width-1:0] yout,
+
+input logic [5:0]   address,      // Address within this peripheral's address space
+input logic [31:0]  data_in,      // Data in to the peripheral, bottom 8, 16 or all 32 bits are valid on write.
+    
+input logic [1:0] data_write_n, // 11 = no write, 00 = 8-bits, 01 = 16-bits, 10 = 32-bits
+input logic [1:0] data_read_n  // 11 = no read,  00 = 8-bits, 01 = 16-bits, 10 = 32-bits
+    
 );
 
 //filtercoefficients
@@ -20,7 +26,8 @@ logic signed [width-1:0] yM [1:N];   // Bandpass reg
 logic signed [width-1:0] yH [1:N];   // Highpass reg
 
 // Memory-mapped registers
-logic [7:0] gL, gM, gH;   // gain registers
+logic filter_en, filter_rst;  // filter  control reg
+logic [7:0] gL, gM, gH;       // gain registers
 // gL = 255: Unity gain (1.0×)  
 // gL = 128: 0.5× (-6dB cut)
 // gL = 64:  0.25× (-12dB cut)
@@ -42,6 +49,10 @@ always_comb
   end
 
 
+// Memory-mapped register interface
+// Address Map:
+
+
 always_ff @(posedge clk or posedge rst)
 	if (rst) begin
 		x[2]  <= 0; x[1]  <= 0; x[0]  <= 0;
@@ -51,12 +62,36 @@ always_ff @(posedge clk or posedge rst)
         gL <= 8'd255;
         gM <= 8'd255;
         gH <= 8'd255;
+        
+        filter_en  <= 1'b0;
 	  end 
 	  
 	else begin // Handle memory-mapped writes
-	    x[2]  <= x[1];  x[1]  <= x[0];  x[0]  <= xin; 
-        yL[1] <= youtL; yM[1] <= youtM; yH[1] <= youtH;
-        yL[2] <= yL[1]; yM[2] <= yM[1]; yH[2] <= yH[1];          
+	   if (~(&data_write_n)) begin
+	       case (address)
+	       
+               6'h00: begin 
+                    x[2]  <= x[1];  x[1]  <= x[0];  x[0]  <= data_in[15:0];
+                    yL[1] <= youtL; yM[1] <= youtM; yH[1] <= youtH;
+                    yL[2] <= yL[1]; yM[2] <= yM[1]; yH[2] <= yH[1];  
+               end 
+               
+               6'h04: begin
+                    if (data_in [28]) 
+                        gL <= data_in[7:0]; 
+                    if (data_in [29]) 
+                        gM <= data_in[15:8];
+                    if (data_in [30]) 
+                        gH <= data_in[23:16];
+                    if (data_in[31]) begin
+                       x[2]  <= 0; x[1]  <= 0; x[0]  <= 0;
+		               yL[1] <= 0; yM[1] <= 0; yH[1] <= 0;
+                       yL[2] <= 0; yM[2] <= 0; yH[2] <= 0; 
+                    end 
+               end           
+               
+          endcase
+       end           
 	end
 	  
 endmodule
--- src/equalizer_isolated/tb.sv
+++ src/equalizer_isolated/tb.sv
@@ -4,21 +4,27 @@ module tb #(parameter width = 16);
 
     logic clk;
     logic rst;
-    logic signed [width-1:0] xin;   
+    logic signed [31:0] xin;   
     logic signed [width-1:0] yout;  
+    
+    logic [5:0]   address;      
+
+    logic [1:0] data_write_n; 
+    logic [1:0] data_read_n;
 
     integer file, outfile, status; 
 	integer sample_count = 0;
 	integer MAX_SAMPLES = 1000;                    
 
-    butter dut ( .clk(clk), .rst(rst), .xin(xin), .yout(yout));
+    butter dut ( .clk(clk), .rst(rst), .data_in(xin), .yout(yout), .address(address), .data_write_n(data_write_n), .data_read_n(data_read_n));
 
     always #20 clk =~clk;
     initial begin
         clk = 0;
         rst = 1;
         xin = 0;
-
+        address = 0;
+        data_write_n = 2'b01;
 	//input and output text files
         file = $fopen("input_signal2.txt", "r");
         if (file == 0) begin
